0d9cdabba81c756f151f04bf4fbb634121bfedd3
stephenchen <stephen.chen@gmail.com>
2018-10-25 22:52:10 -0700

    added POST PUT DELETE webapi

--- restWEBservice11/src/main/java/org/optimate/restWEBservice11/resources/MessageResource.java
+++ restWEBservice11/src/main/java/org/optimate/restWEBservice11/resources/MessageResource.java
@@ -2,7 +2,11 @@ package org.optimate.restWEBservice11.resources;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -12,22 +16,41 @@ import org.optimate.restWEBservice11.model.Message;
 import org.optimate.restWEBservice11.service.MessageService;
 
 @Path("/messages")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class MessageResource {
 	
 	MessageService messageService = new MessageService();
 	
 	
 	@GET
-    @Produces(MediaType.APPLICATION_JSON)
 	public List<Message>  getMessages() {
 		return messageService.getAllMessages();
 	}
 	
+	@POST
+	
+	public Message  addMessages(Message message) {		
+		return messageService.addMessage(message);		
+	}
+	
+	@PUT
+	@Path("/{messageId}")
+	public Message updateMessage(@PathParam("messageId") long id, Message message) {
+		message.setId(id);
+		return messageService.updateMessage(message);
+	}
+	
+	@DELETE
+	@Path("/{messageId}")
+	public void deleteMessage(@PathParam("messageId") long id) {
+		messageService.removeMessage(id);
+	}
+	
 	@GET
 	@Path("/{messageId}")
-	@Produces(MediaType.APPLICATION_JSON)
-	public Message getMessage(@PathParam("messageId") long messageId) {
-		return messageService.getMessage(messageId);
+	public Message getMessage(@PathParam("messageId") long id) {
+		return messageService.getMessage(id);
 	}
 
 
